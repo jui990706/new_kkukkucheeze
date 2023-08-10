d77a51f2349b7dae64ca1c6b417ec5ddb0c2fdb0
dlalsrl0905 <dlalsrl0905@gmail.com>
2023-08-10 11:07:07 +0900

    내꺼다 이자식아 commit

---
+++ 로그인 과제/js/login.js
@@ -0,0 +1,16 @@
+const signInBtn = document.getElementById("signIn");
+const passwordBtn = document.getElementById("password");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+
+signInBtn.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
+
+passwordBtn.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+fistForm.addEventListener("submit", (e) => e.preventDefault());
+secondForm.addEventListener("submit", (e) => e.preventDefault());
